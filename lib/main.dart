ef0e8b2a556de70b4b5b4ced86c67d0a8287167a
RishikeshMane <geeksforrex@gmail.com>
2023-02-24 03:41:58 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:new_app/screens/HomeScreen.dart';
-
-void main() {
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+void main()async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApplication());
 }
 
